ed75f3250cb0c29623834f50e8f58531ce8093f6
CheemsBurguer <strangercheems@gmail.com>
2026-02-09 13:38:01 -0600

    Variables de entorno y migraciones

--- app/core/security.py
+++ app/core/security.py
@@ -1,23 +1,21 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta, timezone
 from passlib.context import CryptContext
+from config import settings
 
-SECRET_KEY = "clave_secreta"
-ALGORITHM = "HS256"
-ACCES_TOKEN_EXPIRE_MINUTES = 30
 
 def crear_token(sub: str, es_admin: bool):
-    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCES_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     data = {
         "sub": sub,
         "exp": expire,
         "es_admin": es_admin
     }
-    return jwt.encode(data, SECRET_KEY, ALGORITHM)
+    return jwt.encode(data, settings.SECRET_KEY, settings.ALGORITHM)
 
 def verificar_token(token: str):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=settings.ALGORITHM)
         return payload
     except JWTError:
         return None
--- app/db/database.py
+++ app/db/database.py
@@ -1,10 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from core.config import settings
 
-DATABASE_URL = "postgresql://postgres:crismo3012@localhost:5432/ecommerce"
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL)
 sessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
 Base = declarative_base()
